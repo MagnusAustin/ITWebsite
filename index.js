615131fd353a6745c955f4175901285241c2dd12
MagnusAustin <111227859+MagnusAustin@users.noreply.github.com>
2023-07-09 19:10:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 let width = screen.width;
 
-if (width <= 414) {
+if (width < 600) {
   document.querySelector(".intro-text").innerHTML = '    <div class="row"><div class="col"><div class="intro"><h1><b>Welcome to Department of Information Technology</b></h1><br><p>The Department is established in 2014 with an intake capacity of 60. In the Information Technology Department, besides imparting theoretical knowledge, a lot of stress is laid on hands-on training and the overall development of the individuals personality.</p></div></div>'
 }
 
-
-opentab(){
-  window.open("https://google.com","_blank")
-}
