722dee50ada639f073b4e3cdfdeb6c611d8ade83
MagnusAustin <111227859+MagnusAustin@users.noreply.github.com>
2023-07-10 19:37:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// document.querySelector("body").style.backgroundColor="black";
+//
+// document.querySelector("body").style.color="green";
+
+
 let width = screen.width;
 
-if (width < 600) {
-  document.querySelector(".intro-text").innerHTML = '    <div class="row"><div class="col"><div class="intro"><h1><b>Welcome to Department of Information Technology</b></h1><br><p>The Department is established in 2014 with an intake capacity of 60. In the Information Technology Department, besides imparting theoretical knowledge, a lot of stress is laid on hands-on training and the overall development of the individuals personality.</p></div></div>'
+if (width < 500) {
+  document.querySelector(".target").innerHTML = '    <div class="row"><div class="col"><div class="intro"><h1><b>Welcome to Department of Information Technology</b></h1><br><p>The Department is established in 2014 with an intake capacity of 60. In the Information Technology Department, besides imparting theoretical knowledge, a lot of stress is laid on hands-on training and the overall development of the individuals personality.</p></div></div>'
 }
-
